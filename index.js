7d5aa3914086f789370f246e76b3150cff694c6d
Ryan Sherrill <ryanbsherrill@gmail.com>
2018-06-10 18:17:32 -0400

    API is parsing request paths

--- index.js
+++ index.js
@@ -3,27 +3,33 @@
 */
 
 // dependencies
-const http = require('http')
-const url = require('url')
+const http = require(`http`)
+const url = require(`url`)
 
 // the server should respond to all requests with a string
 const server = http.createServer((req, res) => {
 
-  // get url and parse
+  // get url and parse it
   const parsedUrl = url.parse(req.url, true)
+  // console.log(parsedUrl)
 
   // get the url path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g,``)
+  // console.log(path)
+  // console.log(trimmedPath)
 
   // send the response
+  res.end(`Hello World!!!\n`)
 
   // log the request path
+  console.log(`Request received on path: ${trimmedPath}`)
 
-  res.end('Yellow Werld!\n')
 })
 
 // start the server and have it listen on port 3000
 const PORT = 3000
 
 server.listen(PORT, () => {
-  console.log(`The server is listening on port ${PORT}`)
+  console.log(`The server is listening on port ${PORT}`)   
 })
