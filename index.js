5bdfa74e31369c6ba8298585b147608937008f5a
Ryan Sherrill <ryanbsherrill@gmail.com>
2018-07-09 03:17:10 -0400

    server is parsing payloads

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-/** Primary file for api */
+/** Primary file for API */
 
 // Dependencies
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // Server should respond to all requests w/ a string
 const server = http.createServer((req, res) => {
@@ -24,17 +25,28 @@ const server = http.createServer((req, res) => {
   // Get headers as an object
   const headers = req.headers
 
-  // Send response
-  res.end('Yellow Werld?\n')
+  // Get payload, if any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  
+  req.on('data', (data) => buffer += decoder.write(data))
+  
+  req.on('end', () => {
+    buffer += decoder.end()
+    
+    // Send response
+    res.end('Yellow Werld?\n')
+
+    // Log payload
+    console.log('Request received w/ payload:', buffer)
+  })
 
   // Log request path, method, qs params, headers
-  console.log(`\nRequest received...
-  path: ${trimmedPath}
-  method: ${method}
-  params:`, queryStringObject,
-  '\n  headers:\n ', headers)
-
-
+  // console.log(`\nRequest received...
+  // path: ${trimmedPath}
+  // method: ${method}
+  // params:`, queryStringObject,
+  // '\n  headers:\n ', headers)
 })
 
 // Start server and listen on PORT 3000
